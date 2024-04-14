c247285c73ca2168f826f208599edc5a31ab9ff1
gurb <pcgezer@hotmail.com>
2024-04-14 19:24:40 +0300

    some additions

---
+++ Application/MappingProfiles/LeaveRequestProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
+using Application.Features.LeaveRequest.Commands.UpdateLeaveRequest;
+using Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
+using Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using AutoMapper;
+using Domain;
+
+
+namespace Application.MappingProfiles;
+
+public class LeaveRequestProfile : Profile
+{
+    public LeaveRequestProfile()
+    {
+        CreateMap<LeaveRequestListDto, LeaveRequest>().ReverseMap();
+        CreateMap<LeaveRequestDetailsDto, LeaveRequest>().ReverseMap();
+        CreateMap<LeaveRequest, LeaveRequestDetailsDto>();
+        CreateMap<CreateLeaveRequestCommand, LeaveRequest>();
+        CreateMap<UpdateLeaveRequestCommand, LeaveRequest>();
+    }
+}
