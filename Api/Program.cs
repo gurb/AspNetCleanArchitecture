6a9a7c01752584d7fd1ea7b4c1d5f7f46b07f371
gurb <pcgezer@hotmail.com>
2024-04-14 19:52:37 +0300

    global error handling

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,4 @@
+using Api.Middleware;
 using Application;
 using Infrastructure;
 using Persistence;
@@ -26,6 +27,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
