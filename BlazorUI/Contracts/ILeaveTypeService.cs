b2872da551b2b72058a641b0d1121b1c457fda7d
gurb <pcgezer@hotmail.com>
2024-04-15 14:44:48 +0300

    setup leave type management service

--- BlazorUI/Contracts/ILeaveTypeService.cs
+++ BlazorUI/Contracts/ILeaveTypeService.cs
@@ -1,6 +1,15 @@
-﻿namespace BlazorUI.Contracts;
+﻿using BlazorUI.Models.LeaveTypes;
+using BlazorUI.Services.Base;
+
+namespace BlazorUI.Contracts;
 
 public interface ILeaveTypeService
 {
+    Task<List<LeaveTypeVM>> GetLeaveTypes();
+    Task<LeaveTypeVM> GetLeaveTypeDetails(int id);
+
+    Task<Response<Guid>> CreateLeaveType(LeaveTypeVM leaveType);
+    Task<Response<Guid>> UpdateLeaveType(int id, LeaveTypeVM leaveType);
+    Task<Response<Guid>> DeleteLeaveType(int id);
 
 }
---
+++ BlazorUI/MappingProfiles/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BlazorUI.Models.LeaveTypes;
+using BlazorUI.Services.Base;
+
+namespace BlazorUI.MappingProfiles;
+
+public class MappingConfig: Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<LeaveTypeDto, LeaveTypeVM>().ReverseMap();
+        CreateMap<CreateLeaveTypeCommand, LeaveTypeVM>().ReverseMap();
+        CreateMap<UpdateLeaveTypeCommand, LeaveTypeVM>().ReverseMap();
+    }
+}
