5f4cfe13b61e6fe597d03eae0807a3cfffe958c2
gurb <pcgezer@hotmail.com>
2024-04-15 09:59:48 +0300

    unit testing handler

---
+++ Application.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using Application.Contracts.Logging;
+using Application.Contracts.Persistence;
+using Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using Application.MappingProfiles;
+using Application.UnitTests.Mocks;
+using AutoMapper;
+using Moq;
+using Shouldly;
+
+
+namespace Application.UnitTests.Features.LeaveTypes.Queries;
+
+public class GetLeaveTypeListQueryHandlerTests
+{
+    private readonly Mock<ILeaveTypeRepository> _mockRepo;
+    private IMapper _mapper;
+    private Mock<IAppLogger<GetLeaveTypesQueryHandler>> _mockAppLogger;
+
+    public GetLeaveTypeListQueryHandlerTests()
+    {
+        _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+        var mapperConfig = new MapperConfiguration(x =>
+        {
+            x.AddProfile<LeaveTypeProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+        _mockAppLogger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+    }
+
+    [Fact]
+    public async Task GetLeaveTypeListTest()
+    {
+        var handler = new GetLeaveTypesQueryHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+
+        var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+        result.ShouldBeOfType<List<LeaveTypeDto>>();
+        result.Count.ShouldBe(3);
+    }
+}
--- Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
+++ Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Contracts.Persistence;
 using Domain;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.UnitTests.Mocks;
 
